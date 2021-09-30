65d5190d83ae9bf5530554639a37edcabe189a69
korlua <90301189+korlua@users.noreply.github.com>
2021-09-30 11:30:35 +0900

    Update Car.java

--- PlayCar/src/Car.java
+++ PlayCar/src/Car.java
@@ -14,6 +14,7 @@ public class Car {
 			this.image = ImageIO.read(new File(imagePath));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
+			// TEST File
 			e.printStackTrace();
 		}
 		this.x = x;
