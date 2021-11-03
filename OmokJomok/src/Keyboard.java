418e45a421155358616e41adaacb1321df291513
kimseongeun06 <teresa119@naver.com>
2021-11-03 23:32:32 +0900

    OmokBoard MyBoard OnePlayerGUI Player Position StoneType Mymatch 장애물판 추가

---
+++ OmokJomok/src/Keyboard.java
@@ -0,0 +1,111 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Keyboard extends JFrame implements KeyListener{
+	Map map;
+	int x=11,y=11;
+	MapSize ms;
+	DrawBorad d;
+	GUI main;
+	
+	public Keyboard(Map m,MapSize ms,DrawBorad d,GUI main) {
+		// TODO Auto-generated constructor stub
+		this.map=m;
+		this.ms=ms;
+		this.d=d;
+		this.main=main;
+	}
+	JLabel k = new JLabel();
+
+	   public void keyPressed(KeyEvent e){
+	    switch(e.getKeyCode()){
+	     //À§
+	     case 38:
+	    	 if(y==1) {
+	    		 return;
+	    	 }
+	    	 if(map.getXY(y-1, x) != 0) {
+	        	   return;
+	           }
+
+	    	 y--;
+	    	 map.setTest(y, x);
+	   	    	d.repaint();
+
+	      break;
+	      
+	     //¾Æ·¡
+	     case 40:
+	    	 if(y==20) {
+	    		 return;
+	    	 }
+	    	 if(map.getXY(y+1, x) != 0) {
+	        	   return;
+	           }
+
+	    	 y++;
+	    	 map.setTest(y, x);
+	   	    	d.repaint();
+
+	      break;
+	      
+	     //¿Þ¦U
+	     case 37:
+	    	 if(x==1) {
+	    		 return;
+	    	 }
+	    	 if(map.getXY(y, x-1) != 0) {
+	        	   return;
+	           }
+
+	    	 x--;
+	    	 map.setTest(y, x);
+	   	    	d.repaint();
+
+	      break;
+	      
+	     //¿À¸¥¦U
+	     case 39:
+	    	 if(x==20) {
+	    		 return;
+	    	 }
+	    	 if(map.getXY(y, x+1) != 0) {
+	        	   return;
+	           }
+
+	    	 x++;
+	    	 map.setTest(y, x);
+	   	    	d.repaint();
+
+	      break;
+	      
+	     case 32:
+	    	map.setMap(y, x);
+	 	    map.changeCheck();
+	 	    d.repaint();
+	 	    if(map.winCheck(x, y))
+	 	    	if(map.getCheck()==true)
+	 	    		main.showPopUp("¹éµ¹ ½Â¸®!");
+	 	    	else
+	 	    		main.showPopUp("Èæµ¹ ½Â¸®!");
+
+	   
+	   }
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+}
+
+
---
+++ OmokJomok/src/OnePlayerGUI.java
@@ -0,0 +1,58 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.SystemColor;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.GroupLayout;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.border.EmptyBorder;
+
+public class OnePlayerGUI extends JDialog {
+	private SingleGameGUI sGame;
+	private final JPanel contentPanel = new JPanel();
+	private JTextField textField;
+	private JTextField textField_1;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			OnePlayerGUI dialog = new OnePlayerGUI();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public OnePlayerGUI() {
+		// ID
+		int id = 001;
+		// 이름
+		String name = "Player1";
+		// 순서
+		int order = 1;
+
+		// 오목판 크기(0=11x11, 1=15x15, 2=19x19)
+		int size = 2;
+
+
+		sGame = new SingleGameGUI(id, name, order, size);
+
+	}
+}
